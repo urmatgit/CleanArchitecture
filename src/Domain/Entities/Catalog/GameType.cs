73335f71034fd4b3487b7cb3496eb875fb834560
iUrmat <urmat2g@gmail.com>
2021-06-30 18:47:37 +0300

    add enitities

---
+++ src/Domain/Entities/Catalog/GameType.cs
@@ -0,0 +1,16 @@
+﻿using BlazorHero.CleanArchitecture.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Domain.Entities.Catalog
+{
+    /// <summary>
+    /// Game, Workout,GameTraining,
+    /// </summary>
+    public class GameType : BaseEntity
+    {
+    }
+}
